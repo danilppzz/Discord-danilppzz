40213ef5cc26abbb5277a6edd2d5d3f293a149a2
d4niz <josedanielmorebolopez@gmail.com>
2023-10-18 12:26:27 +0200

    UpdateName Checker

---
+++ src/main/java/dev/soizx/handler/GuildMemberNameCheckerHandler.java
@@ -0,0 +1,17 @@
+package dev.soizx.handler;
+
+import dev.soizx.Main;
+import net.dv8tion.jda.api.events.guild.member.update.GuildMemberUpdateNicknameEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+public class GuildMemberNameCheckerHandler extends ListenerAdapter {
+
+    @Override
+    public void onGuildMemberUpdateNickname(GuildMemberUpdateNicknameEvent event) {
+        String oldNickName = event.getOldNickname();
+        String newNickName = event.getNewNickname();
+        String userID = event.getMember().getId();
+        Main.logger.info("User " + userID + " changed nickname from " + oldNickName + " to " + newNickName);
+    }
+}
+
